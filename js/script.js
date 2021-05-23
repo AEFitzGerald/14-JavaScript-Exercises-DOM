df45f9d8944e22e4069c8c839b4dc881ef4dbe72
Anna FitzGerald <46199551+AEFitzGerald@users.noreply.github.com>
2021-05-23 09:20:49 -0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+//change the color of the instagram icon
+var iconInstagram = document.querySelector(".fa-instagram");
+iconInstagram.style.color = "#d45bb9";
+//console.log(iconInstagram);
+
+//change the color of the facebook icon
+var iconFacebook = document.querySelector(".fa-facebook");
+iconFacebook.style.color = "#6a99b5";
+//console.log(iconFacebook);
+
+//apply border to hours open in contact section
+var hoursOpenBorder = document.querySelector(".contact p");
+hoursOpenBorder.style.border = "1px solid #d45bb9";
+//console.log(hoursOpenBorder)
+
+//apply padding to hours open border
+var hoursOpenPadding = document.querySelector(".contact p");
+hoursOpenPadding.style.padding = "1em";
+//console.log(hoursOpenPadding);
